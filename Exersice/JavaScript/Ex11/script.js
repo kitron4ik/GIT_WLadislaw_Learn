6ee7dcd0fe917798932b74ca70152bc96856358e
kitron4ik <craftukww@gmail.com>
2023-09-09 19:16:51 +0300

    Вёрстка Ex 1-15 и JavaScript 1-15

---
+++ Exersice/JavaScript/Ex11/script.js
@@ -0,0 +1,28 @@
+//Задание №1
+let a = 10
+let b = 2
+alert(a+b)
+alert(a-b)
+alert(a*b)
+alert(a/b)
+
+//Задание №2
+let c = 10
+let d = 5
+let result = c+d
+alert(result)
+
+//Задание №3
+a = 1
+b = 2
+c = 3
+let sum = a+b+c
+alert(sum)
+
+//Задание №4
+a = 10
+b = 5
+c = a-b
+d = 7
+result = c+d
+alert(result)
